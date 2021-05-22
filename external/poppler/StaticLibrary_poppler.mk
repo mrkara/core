8c52af6f018b2c2d902237f17bb17b42bddfef24
Jan-Marek Glogowski <glogow@fbihome.de>
2021-05-22 13:30:41 +0200

    no fontconfig
    Change-Id: Ica735004d61040651c6ce8b9fd863728c8027495

--- external/poppler/StaticLibrary_poppler.mk
+++ external/poppler/StaticLibrary_poppler.mk
@@ -11,10 +11,7 @@ $(eval $(call gb_StaticLibrary_StaticLibrary,poppler))
 
 $(eval $(call gb_StaticLibrary_use_unpacked,poppler,poppler))
 
-$(eval $(call gb_StaticLibrary_use_externals,poppler,\
-    fontconfig \
-    libjpeg \
-))
+$(eval $(call gb_StaticLibrary_use_external,poppler,libjpeg))
 
 $(eval $(call gb_StaticLibrary_set_warnings_disabled,poppler))
 
